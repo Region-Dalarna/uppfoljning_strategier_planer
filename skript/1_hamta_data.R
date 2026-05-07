664fb032dc8608b62df5f22ae30f517887fb8bbd
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2026-05-07 14:55:21 +0200

    Diverse mindre justeringar

--- skript/1_hamta_data.R
+++ skript/1_hamta_data.R
@@ -27,37 +27,36 @@ if(uppdatera_data == TRUE){
     
     cat("Hämtning av data påbörjad\n")
     start_time <- Sys.time()
-    
 
-      skriptrader_upprepa_om_fel({
-      source("https://raw.githubusercontent.com/Region-Dalarna/uppfoljning_dalastrategin/refs/heads/main/Skript/diagram_skogsmark.R")
-        gg_skogsmark <- diagram_skogsmark(region = "20",
-                                          output_mapp = output_mapp_figur,
-                                          returnera_data = TRUE,
-                                          diag_areal = TRUE,
-                                          diag_andel = TRUE,
-                                          ggobjektfilnamn_utan_tid = TRUE,
-                                          spara_figur = spara_figurer)
-        
-        ar_min_skogsmark <- as.character(min(skogsmark_df$år))
-        ar_max_skogsmark <- as.character(max(skogsmark_df$år))
-        
-        areal_skogsmark_forandring_procent <-round(((skogsmark_df %>% filter(år==max(år)) %>% .$area_hektar/skogsmark_df %>% filter(år==min(år)) %>% .$area_hektar)-1)*100,0)
-        areal_skogsmark_min_ar <- format(skogsmark_df %>% filter(år==min(år)) %>% .$area_hektar,big.mark=" ")
-        areal_skogsmark_max_ar <- format(skogsmark_df %>% filter(år==max(år)) %>% .$area_hektar,big.mark=" ")
-        
-        andel_skogsmark_min_ar <- round(skogsmark_df %>% filter(år==min(år)) %>% .$area_procent,0)
-        andel_skogsmark_max_ar <- round(skogsmark_df %>% filter(år==max(år)) %>% .$area_procent,0)
+    skriptrader_upprepa_om_fel({
+    source("https://raw.githubusercontent.com/Region-Dalarna/uppfoljning_dalastrategin/refs/heads/main/Skript/diagram_skogsmark.R")
+      gg_skogsmark <- diagram_skogsmark(region = "20",
+                                        output_mapp = output_mapp_figur,
+                                        returnera_data = TRUE,
+                                        diag_areal = TRUE,
+                                        diag_andel = TRUE,
+                                        ggobjektfilnamn_utan_tid = TRUE,
+                                        spara_figur = spara_figurer)
       
-    })
-  
-    # Sparar global environment i R. Detta för att man skall slippa hämta data varje gång
-    save.image(file = glue("{mapp_environment_fil}{repo_namn}.RData"))
+      ar_min_skogsmark <- as.character(min(skogsmark_df$år))
+      ar_max_skogsmark <- as.character(max(skogsmark_df$år))
+      
+      areal_skogsmark_forandring_procent <-round(((skogsmark_df %>% filter(år==max(år)) %>% .$area_hektar/skogsmark_df %>% filter(år==min(år)) %>% .$area_hektar)-1)*100,0)
+      areal_skogsmark_min_ar <- format(skogsmark_df %>% filter(år==min(år)) %>% .$area_hektar,big.mark=" ")
+      areal_skogsmark_max_ar <- format(skogsmark_df %>% filter(år==max(år)) %>% .$area_hektar,big.mark=" ")
+      
+      andel_skogsmark_min_ar <- round(skogsmark_df %>% filter(år==min(år)) %>% .$area_procent,0)
+      andel_skogsmark_max_ar <- round(skogsmark_df %>% filter(år==max(år)) %>% .$area_procent,0)
     
-    end_time <- Sys.time()
-    elapsed_time <- as.numeric(difftime(end_time, start_time, units = "mins"))
-    cat(sprintf("Hämtning av data klar: Det tog %.2f minuter.", elapsed_time))
-    cat("\n\n")
+  })
+
+  # Sparar global environment i R. Detta för att man skall slippa hämta data varje gång
+  save.image(file = glue("{mapp_environment_fil}{repo_namn}.RData"))
+  
+  end_time <- Sys.time()
+  elapsed_time <- as.numeric(difftime(end_time, start_time, units = "mins"))
+  cat(sprintf("Hämtning av data klar: Det tog %.2f minuter.", elapsed_time))
+  cat("\n\n")
     
     
   } else {
